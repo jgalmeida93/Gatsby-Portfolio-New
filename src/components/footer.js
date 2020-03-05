96f5ee493e637c17f09d94d238056de39ece848a
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-05 12:14:16 -0300

    :iphone: Corrige responsividade do footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,28 +11,26 @@ const Footer = () => {
         <footer>
             <div className="footer">
 
-                <div className="row">
+                <div className="row m-0">
                         <div className="logo-footer w-100 text-center py-3">
                             <span className="logo-decoration"></span><strong>JGA</strong>LMEIDA
                         </div>
                 </div>
 
-                <div className="row">
-                    <div className="white-row">
-                        <div className="row my-4">                            
-                        <div className="col-4 text-center">
+                <div className="white-row">
+                        <div className="row mx-0 my-4">                            
+                        <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 text-center">
                                 <a href="https://github.com/jgalmeida93" className="social-footer"><FaGithubAlt size="2em"/></a>    
                             </div>
-                            <div className="col-4 text-center">
+                            <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 text-center">
                                 <a href="https://www.linkedin.com/in/jonas-gabriel-de-almeida-4ab460187/" className="social-footer">
                                         <FaLinkedin size="2em"/>
                                 </a>
                             </div>
-                            <div className="col-4 text-center">
+                            <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 text-center">
                                 <a href="https://www.instagram.com/jgalmeida93/" className="social-footer"><FaInstagram size="2em"/></a>
                             </div>
                         </div>
-                    </div>
                 </div>
 
             </div>
