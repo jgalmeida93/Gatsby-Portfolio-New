331ac1eddbb615310d779b524c794e943b5666a1
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-05 08:54:34 -0300

    :iphone: Corrigi responsividade do menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Link } from "gatsby";
 import '../styles/style.scss'
 
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap"
+import { Navbar, Nav } from "react-bootstrap"
 
 const Menu = () => {
  
   return (
-    <Navbar className="navbar-dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" className="navbar-dark" variant="dark">
       <Navbar.Brand href="/" className="logo"><span className="logo-decoration"></span><strong>JGA</strong>LMEIDA</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" >
         <Nav className="mr-auto">
           <Nav.Link href="#sobre">SOBRE</Nav.Link>
           <Nav.Link href="#trabalhos">TRABALHOS</Nav.Link>
           <Nav.Link href="#contato">CONTATO</Nav.Link>
         </Nav>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
