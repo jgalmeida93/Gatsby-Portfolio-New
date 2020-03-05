390f9091cdf4c0b434aecf5752ca3726a9698ef6
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-04 22:16:00 -0300

    🌟 Cria Header component

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,10 @@ import React from "react"
 
 const Header = () => {
   return  (
-    <div className="header">
-      alou
+    <div className="row menu-header">
+    <div className="col-12 align-self-center">
+  <h1 className="m-auto banner-text"><span className="bracket">{`{`}</span>FRONTEND DEVELOPER<span className="bracket">{`}`}</span></h1>
+    </div>
     </div>
   )
 }
