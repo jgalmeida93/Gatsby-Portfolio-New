2bc615ce70d8c309b22052e2715a5c6986c0d6f5
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-04 22:16:59 -0300

    🌟 Cria Trabalhos component

---
+++ src/components/trabalhos.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Trabalhos = () => {
+    return (
+        <div className="container">
+                <h2>TRABALHOS</h2> 
+                <br/>
+                <h5>Aqui estão alguns dos trabalhos que desenvolvi</h5>
+                <div className="row">
+                    <div className="col-6 my-3 align-self-center">
+                        <div className="border d-block bg-dark trabalhos rounded">
+                            <h4 className="trabalhos-texto d-block">Em breve</h4>
+                        </div>
+                        <small>Legenda</small>
+                    </div>
+                    <div className="col-6 my-3 align-self-center">
+                    <div className="border d-block bg-dark trabalhos rounded">
+                        <h4 className="trabalhos-texto d-block">Em breve</h4>
+                    </div>
+                    <small>Legenda</small>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6 my-3 align-self-center">
+                    <div className="border d-block bg-dark trabalhos rounded">
+                        <h4 className="trabalhos-texto d-block">Em breve</h4>
+                        </div>
+                        <small>Legenda</small>
+                    </div>
+                    <div className="col-6 my-3 align-self-center">
+                    <div className="border d-block bg-dark trabalhos rounded">
+                        <h4 className="trabalhos-texto d-block">Em breve</h4>
+                        </div>
+                        <small>Legenda</small>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default Trabalhos;
